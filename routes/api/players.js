b6ea59c657f56d322d9c74b121d22d06e3c43edd
Rob Mercier <rob.mercier@pureincubation.com>
2024-07-07 20:43:10 -0400

    Update stats page to show current season

--- routes/api/players.js
+++ routes/api/players.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const ObjectId = require('mongoose').Types.ObjectId;
 
 const Players = require('../../models/Players');
+const Seasons = require('../../models/Seasons');
+const Teams = require('../../models/Teams');
 
 /**
  * @route GET players
  * @description Get all players
  * @access Public
  */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const isCurrentSeason = req.query.isCurrentSeason;
   const isGoalie = req.query.isGoalie;
   let params = { isGoalie: isGoalie };
 
@@ -16,23 +20,53 @@ router.get('/', (req, res) => {
     params = {}
   }
 
-  Players.find(params)
-    .then(
-      (players) => {
-        for (let player of players) {
-          if (isGoalie) {
-            // Calculate advanced goalie stats
-            if (typeof player.shotsAgainst !== 'undefined') {
-              player.goalsAgainst = (+player.shotsAgainst - +player.saves);
-              player.savePercentage = ((+player.goalsAgainst) / (+player.shotsAgainst)).toFixed(2);
+  if (isCurrentSeason) {
+    const seasonId = await Seasons.findOne({ currentSeason: isCurrentSeason })
+      .then(
+        (season) => {
+          return (season === null) ? null : season._id;
+        }
+      )
+      .catch(err => res.status(404).json({
+        noSeasonsFound: 'No Current Season Found'
+      }));
+
+    await Teams.find({ season: seasonId })
+      .then(
+        (teams) => {  
+          const seasonPlayers = [];
+
+          for (let team of teams) {
+            for (let player of team.players) {
+              if ((!player.isGoalie && isGoalie === 'false') || (player.isGoalie && isGoalie !== 'false')) {
+                seasonPlayers.push(player);
+              }
             }
           }
-        }
 
-        res.json(players)
-      }
-    )
-    .catch(err => res.status(404).json({ noPlayersfound: 'No Players found' }));
+          res.json(seasonPlayers);
+        }
+      )
+      .catch(err => res.status(404).json({ noSchedulesfound: 'No Teams found' }));
+  } else {
+    Players.find(params)
+      .then(
+        (players) => {
+          for (let player of players) {
+            if (isGoalie) {
+              // Calculate advanced goalie stats
+              if (typeof player.shotsAgainst !== 'undefined') {
+                player.goalsAgainst = (+player.shotsAgainst - +player.saves);
+                player.savePercentage = ((+player.goalsAgainst) / (+player.shotsAgainst)).toFixed(2);
+              }
+            }
+          }
+
+          res.json(players)
+        }
+      )
+      .catch(err => res.status(404).json({ noPlayersfound: 'No Players found' }));
+  }
 });
 
 /**
